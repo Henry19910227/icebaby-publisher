bfbcf5f2dae7be8710a5a40d9e4b1b263943423c
henry <toyokoyo199@gmail.com>
2020-11-21 12:39:22 +0800

    新增 drink 欄位

--- icebaby-user-mysql-volumes/initdb/icebaby.sql
+++ icebaby-user-mysql-volumes/initdb/icebaby.sql
@@ -8,7 +8,7 @@ CREATE TABLE users (
    create_at DATETIME NOT NULL DEFAULT NOW(),
    update_at DATETIME NOT NULL DEFAULT NOW(),
    `status` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0'
-) AUTO_INCREMENT = 10001
+) AUTO_INCREMENT = 10001;
 
 CREATE TABLE user_details (
    id INT(11) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
@@ -24,8 +24,9 @@ CREATE TABLE user_details (
    weight tinyint(3) UNSIGNED NOT NULL DEFAULT '0',
    favorite VARCHAR(50) NOT NULL DEFAULT '',
    smoke tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
+   drink tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
    CONSTRAINT fk_details_users FOREIGN KEY (user_id) REFERENCES users(id)
-)
+);
 
 CREATE TABLE user_auths (
    id INT(11) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
@@ -34,4 +35,4 @@ CREATE TABLE user_auths (
 	 identifier VARCHAR(50) NOT NULL DEFAULT '',
 	 `password` VARCHAR(10) NOT NULL DEFAULT '',
 	CONSTRAINT fk_auths_users FOREIGN KEY (user_id) REFERENCES users(id)
-)
+);
